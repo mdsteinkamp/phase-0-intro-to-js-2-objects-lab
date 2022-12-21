b59c5ec9bdbc52234bc3059eb26a795a91663de4
Matthew Steinkamp <mdsteinkamp@gmail.com>
2022-12-20 18:54:47 -0800

    commit tree 9d67dc459087add883500e96af47f76e61e55b0d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Employee 1", 
+    streetAddress: "123 Fake Street",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const updatedEmployee = {...object};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const updatedEmployee = {...object};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+}
